157371484ecf52256824356ee23bb5836fdbddf5
ZetZhang <13660591402@163.com>
2023-03-26 00:37:01 +0800

    test eunomia

---
+++ test/eunomia/execsnoop/execsnoop.h
@@ -0,0 +1,13 @@
+#ifndef __EXECSNOOP_H
+#define __EXECSNOOP_H
+
+#define TASK_COMM_LEN 16
+
+struct event {
+ int pid;
+ int ppid;
+ int uid;
+ char comm[TASK_COMM_LEN];
+};
+
+#endif /* __EXECSNOOP_H */
