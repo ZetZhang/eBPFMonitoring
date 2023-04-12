5411a0c37eee3dcf3909837d8fe1de931d64369c
ZetZhang <13660591402@163.com>
2023-04-13 01:45:00 +0800

    exporter BPF

---
+++ src/exporter/oomkill.c
@@ -0,0 +1,107 @@
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <bpf/libbpf.h>
+#include "oomkill.skel.h"
+
+int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	/* Ignore debug-level libbpf logs */
+	if (level > LIBBPF_INFO)
+		return 0;
+	return vfprintf(stderr, format, args);
+}
+
+void bump_memlock_rlimit(void)
+{
+	struct rlimit rlim_new = {
+		.rlim_cur	= RLIM_INFINITY,
+		.rlim_max	= RLIM_INFINITY,
+	};
+
+	if (setrlimit(RLIMIT_MEMLOCK, &rlim_new)) {
+		fprintf(stderr, "Failed to increase RLIMIT_MEMLOCK limit!\n");
+		exit(1);
+	}
+}
+
+static volatile bool exiting = false;
+
+static void sig_handler(int sig)
+{
+	exiting = true;
+}
+
+void handle_event(void *ctx, int cpu, void *data, unsigned int data_sz)
+{
+	const struct event *e = data;
+	printf("yes\n");
+}
+
+int main(int argc, char **argv) 
+{
+	struct perf_buffer *pb = NULL;
+    struct oomkill_bpf *skel;
+    struct perf_buffer_opts pb_opts = {};
+	int err;
+
+    /* Set up libbpf logging callback */
+	libbpf_set_print(libbpf_print_fn);
+
+	/* Bump RLIMIT_MEMLOCK to create BPF maps */
+	bump_memlock_rlimit();
+
+	/* Clean handling of Ctrl-C */
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+
+    /* Load and verify BPF application */
+	skel = oomkill_bpf__open_and_load();
+	if (!skel) {
+		fprintf(stderr, "Failed to open and load BPF skeleton\n");
+		return 1;
+	}
+
+    /* Attach tracepoint */
+	err = oomkill_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+    /* Set up ring buffer polling */
+	pb_opts.sample_cb = handle_event;
+	pb = oomkill_bpf__new(bpf_map__fd(skel->maps.oom_kills_total), 8 /* 32KB per CPU */, &pb_opts);
+	if (libbpf_get_error(pb)) {
+		err = -1;
+		fprintf(stderr, "Failed to create perf buffer\n");
+		goto cleanup;
+	}
+
+	/* Attach tracepoint */
+	err = oomkill_bpf__attach(skel);
+	if (err) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+while (!exiting) {
+		err = perf_buffer__poll(pb, 100 /* timeout, ms */);
+		/* Ctrl-C will cause -EINTR */
+		if (err == -EINTR) {
+			err = 0;
+			break;
+		}
+		if (err < 0) {
+			printf("Error polling perf buffer: %d\n", err);
+			break;
+		}
+	}
+
+cleanup:
+	perf_buffer__free(pb);
+	perfbuf_output_bpf__destroy(skel);
+    return 0;
+}
